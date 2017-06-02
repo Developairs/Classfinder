e94e21a8022d0792c822bbe1561fcf4f0c7ab913
Nikos <silverflyingdragon@yahoo.gr>
2017-06-02 14:33:00 +0300

    diafores allages oste na doulevei k genika update

--- app/src/main/java/com/mouratidis/nikos/classfinder/app/AppConfig.java
+++ app/src/main/java/com/mouratidis/nikos/classfinder/app/AppConfig.java
@@ -2,8 +2,8 @@ package com.mouratidis.nikos.classfinder.app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.1.2/android_login_api/login.php";
+    public static String URL_LOGIN = "http://192.168.1.3/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.1.2/android_login_api/register.php";
+    public static String URL_REGISTER = "http://192.168.1.3/android_login_api/register.php";
 }
